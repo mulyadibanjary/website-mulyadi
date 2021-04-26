4a9bf6591e7c3e3e734aea55021eea1ce9955669
mulyadibanjary <mulyadi.mulyadi9620@jatis.com>
2021-04-26 12:25:28 +0700

    menambahkan detail dan index.php

---
+++ app/views/mahasiswa/index.php
@@ -0,0 +1,54 @@
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-6">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+            Tambah Mahasiswa
+        </button><br><br>
+            <ul>Daftar Mahasiswa</ul>
+                <ul class="list-group">
+                    <?php   foreach($data['mhs'] as $mhs):?>
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <?= $mhs['nama_lengkap']?>
+                    <a href="<?= BASEURL;?>/mahasiswa/detail/<?= $mhs['id'];?>" class="badge bg-primary">Detail</a>
+                    </li>
+                    <?php endforeach;?>
+               </ul>
+        </div>
+    </div>
+    <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah data mahasiswa</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+    
+        <form action="<?= BASEURL;?>/mahasiswa/insert" method="post">
+        <div class="mb-3">
+        <label for="nama_lengkap" class="form-label">Nama Lengkap</label>
+        <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" placeholder="Nama Lengkap">
+        </div>
+        <div class="mb-3">
+        <label for="nrp" class="form-label">NRP</label>
+        <input type="number" class="form-control" id="nrp" name="nrp" placeholder="NRP">
+        </div>
+        <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="example@email.com">
+        </div>
+        <div class="mb-3">
+        <label for="jurusan" class="form-label">Jurusan</label>
+        <input type="text" class="form-control" id="jurusan" name="jurusan" placeholder="Jurusan">
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
